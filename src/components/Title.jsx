af205a1568246041372d7f8a254ffbdeee7cac1d
Vladislav Diachenko <Vladyslav_Diachenko1@epam.com>
2022-12-02 09:04:48 +0200

    project init

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+  return (
+    <div className='Title'>
+        <h1>Катя какашка</h1>
+       
+            <h2>San Franciscos Privately Owned Public Spaces</h2>
+    </div>
+  )
+}
+
+export default Title
